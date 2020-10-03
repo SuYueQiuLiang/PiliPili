4607b92cd15b47a9c6140c33b5c3a4a7f4da44cc
SuYueQiuLiang <qiuliangsuyue@163.com>
2020-10-03 23:26:44 +0800

    完成url获取方法

--- app/src/main/java/org/suyueqiuliang/pilipili/MainActivity.java
+++ app/src/main/java/org/suyueqiuliang/pilipili/MainActivity.java
@@ -1,8 +1,12 @@
 package org.suyueqiuliang.pilipili;
 
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,19 +14,59 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+        //AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        //        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+        //        .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-    }
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+        Resources resource= getBaseContext().getResources();
+        @SuppressLint("ResourceType") ColorStateList csl= getColorStateList(R.animator.navigation_menu_item_color);
+        navView.setItemTextColor(csl);
+        navView.setItemIconTintList(csl);
 
+
+
+    }
+    private StringBuffer getAPIReturn(String urlStr){
+        StringBuffer document = new StringBuffer();
+        try {
+            //链接URL
+            URL url = new URL(urlStr);
+            //返回结果集
+            //创建链接
+            URLConnection conn = url.openConnection();
+            //读取返回结果集
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                document.append(line);
+            }
+            reader.close();
+            System.out.println(document);
+        }catch (UnsupportedEncodingException | MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return document;
+    }
 }
