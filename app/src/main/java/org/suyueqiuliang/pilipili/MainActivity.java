70d91eace3de1dce8c04ff4844233051973a879d
SuYueQiuLiang <qiuliangsuyue@163.com>
2020-10-08 14:55:17 +0800

    完成登陆二维码生成函数，优化部分算法

--- app/src/main/java/org/suyueqiuliang/pilipili/MainActivity.java
+++ app/src/main/java/org/suyueqiuliang/pilipili/MainActivity.java
@@ -29,12 +29,15 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
 import org.apache.commons.codec.binary.Base64;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -90,28 +93,27 @@ public class MainActivity extends AppCompatActivity {
                 final EditText inputUserName = dialogView.findViewById(R.id.input_user_name);
                 final EditText inputUserPassword = dialogView.findViewById(R.id.input_user_password);
                 final ImageView qr_core = dialogView.findViewById(R.id.qr_code);
-                try {
-                    Map<EncodeHintType, String> hints = new HashMap<>();
-                    hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-                    BitMatrix bitMatrix = new MultiFormatWriter().encode("https://passport.bilibili.com/qrcode/h5/login?oauthKey=5feda1702b81460e9a18a4eb3a416553", BarcodeFormat.QR_CODE, 300, 300, hints);
-                    int width=300,height=300;
-                    int[] pixels = new int[width * height];
-                    for (int y = 0; y < height; y++) {
-                        for (int x = 0; x < width; x++) {
-                            //bitMatrix.get(x,y)方法返回true是黑色色块，false是白色色块
-                            if (bitMatrix.get(x, y)) {
-                                pixels[y * width + x] =  Color.BLACK;//黑色色块像素设置
-                            } else {
-                                pixels[y * width + x] =  Color.WHITE;// 白色色块像素设置
-                            }
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            String apiReturn = getNoContentAPIReturn("https://passport.bilibili.com/qrcode/getLoginUrl");
+                            JSONObject jsonObject = new JSONObject(apiReturn);
+                            JSONObject newob = jsonObject.getJSONObject("data");
+                            String url = newob.getString("url");
+                            String oauthKey = newob.getString("oauthKey");
+                            final Bitmap bitmap = makeQrCore(url);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    qr_core.setImageBitmap(bitmap);
+                                }
+                            });
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
                     }
-                    Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-                    bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
-                    qr_core.setImageBitmap(bitmap);
-                } catch (WriterException e) {
-                    e.printStackTrace();
-                }
+                }).start();
                 loggingButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -136,18 +138,15 @@ public class MainActivity extends AppCompatActivity {
                 customizeDialog.show();
             }
         });
-
-        
     }
-    private String getAPIReturn(String urlStr){
+
+    //一般无正文api获取返回值
+
+    private String getNoContentAPIReturn(String urlStr){
         StringBuffer document = new StringBuffer();
         try {
-            //链接URL
             URL url = new URL(urlStr);
-            //返回结果集
-            //创建链接
             URLConnection conn = url.openConnection();
-            //读取返回结果集
             BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
             String line;
             while ((line = reader.readLine()) != null) {
@@ -163,9 +162,114 @@ public class MainActivity extends AppCompatActivity {
         return document.toString();
     }
 
+    //带正文api获取返回值
+
+    private String getWithContentAPIReturn(String urlStr,String content) {
+// TODO Auto-generated method stub
+        StringBuffer document = new StringBuffer();
+        boolean status = false;
+        try {
+            //链接URL
+            URL url = new URL(urlStr);
+            //返回结果集
+            //创建链接
+            URLConnection conn = url.openConnection();
+            //读取返回结果集
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("connection", "keep-alive");
+            conn.setUseCaches(false);//设置不要缓存
+            OutputStreamWriter out =out = new OutputStreamWriter(conn.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+            String line;
+            out.write(content);
+            out.flush();;
+            while ((line = reader.readLine()) != null) {
+                document.append(line);
+            }
+            toast(document.toString());
+            reader.close();
+            out.close();
+        }catch (UnsupportedEncodingException | MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return document.toString();
+    }
+
+    //使用String生成二维码
+
+    private Bitmap makeQrCore(String string){
+        Bitmap bitmap = null;
+        try {
+            Map<EncodeHintType, String> hints = new HashMap<>();
+            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+            BitMatrix bitMatrix = null;
+            bitMatrix = new MultiFormatWriter().encode(string, BarcodeFormat.QR_CODE, 300, 300, hints);
+
+            final int width=300,height=300;
+            final int[] pixels = new int[width * height];
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    //bitMatrix.get(x,y)方法返回true是黑色色块，false是白色色块
+                    if (bitMatrix.get(x, y)) {
+                        pixels[y * width + x] =  Color.BLACK;//黑色色块像素设置
+                    } else {
+                        pixels[y * width + x] =  Color.WHITE;// 白色色块像素设置
+                    }
+                }
+            }
+            bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void toast(String s){
+        System.out.println(s);
+    }
+
     private String encryption(String password){
         try {
-            String returnString = getAPIReturn("https://passport.bilibili.com/login?act=getkey");
+            String returnString = getNoContentAPIReturn("https://passport.bilibili.com/login?act=getkey");
             int n1 = 0, n2, i = 3;
             String hash, publicKey;
             while (i-- != 0) {
