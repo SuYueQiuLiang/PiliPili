890177d6580987413631289eac5c39414b58cd5a
SuYueQiuLiang <qiuliangsuyue@163.com>
2020-10-09 14:38:48 +0800

    完善用户数据保存读取接口，实现启动自动读取识别文件并尝试登陆

--- app/src/main/java/org/suyueqiuliang/pilipili/MainActivity.java
+++ app/src/main/java/org/suyueqiuliang/pilipili/MainActivity.java
@@ -1,7 +1,10 @@
 package org.suyueqiuliang.pilipili;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -16,6 +19,8 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -25,7 +30,6 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
 import org.apache.commons.codec.binary.Base64;
@@ -33,12 +37,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -51,19 +60,30 @@ import java.util.Map;
 
 import javax.crypto.Cipher;
 
+class UserData{
+    String DedeUserID,DedeUserID__ckMd5,Expires,SESSDATA,bili_jct;
+}
+
 public class MainActivity extends AppCompatActivity {
 
+    boolean wasLogin=false,keepLoginListen=false;
+    String localFilePath;
+    UserData userData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //申请权限
+
+        requestMyPermissions();
+
         //控件变量
 
+        localFilePath = MainActivity.this.getExternalFilesDir("res")+"/";
+        toast(localFilePath);
         NavigationView navView = findViewById(R.id.nav_view);
         View navHead = navView.getHeaderView(0);
         ImageView userHead = navHead.findViewById(R.id.user_head);
-
-        //加密字符串
 
         //侧边栏和状态栏初始化
 
@@ -78,64 +98,104 @@ public class MainActivity extends AppCompatActivity {
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         getWindow().setStatusBarColor(getResources().getColor(R.color.colorAccent));
 
+        //尝试从本地读取数据登陆
+        userData = readUserInfo();
+        if(userData!=null){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    wasLogin = testLogin(userData.SESSDATA);
+                    if(wasLogin);
+                }
+            }).start();
+        }
+
         //监听事件
         userHead.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder customizeDialog =
-                        new AlertDialog.Builder(MainActivity.this);
-                final View dialogView = LayoutInflater.from(MainActivity.this)
-                        .inflate(R.layout.loging_dialog,null);
-                customizeDialog.setView(dialogView);
+                if (!wasLogin) {
+                    AlertDialog.Builder customizeDialog =
+                            new AlertDialog.Builder(MainActivity.this);
+                    final View dialogView = LayoutInflater.from(MainActivity.this)
+                            .inflate(R.layout.loging_dialog, null);
+                    customizeDialog.setView(dialogView);
 
-                final ProgressBar loggingProgressBar = dialogView.findViewById(R.id.logging_progressBar);
-                Button loggingButton = dialogView.findViewById(R.id.logging_button);
-                final EditText inputUserName = dialogView.findViewById(R.id.input_user_name);
-                final EditText inputUserPassword = dialogView.findViewById(R.id.input_user_password);
-                final ImageView qr_core = dialogView.findViewById(R.id.qr_code);
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            String apiReturn = getNoContentAPIReturn("https://passport.bilibili.com/qrcode/getLoginUrl");
-                            JSONObject jsonObject = new JSONObject(apiReturn);
-                            JSONObject newob = jsonObject.getJSONObject("data");
-                            String url = newob.getString("url");
-                            String oauthKey = newob.getString("oauthKey");
-                            final Bitmap bitmap = makeQrCore(url);
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    qr_core.setImageBitmap(bitmap);
-                                }
-                            });
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }).start();
-                loggingButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        new Thread(new Runnable() {
-                            @Override
-                            public void run() {
-                                String userName = inputUserName.getText().toString();
-                                String userPassword = inputUserPassword.getText().toString();
+                    final ProgressBar loggingProgressBar = dialogView.findViewById(R.id.logging_progressBar);
+                    Button loggingButton = dialogView.findViewById(R.id.logging_button);
+                    final EditText inputUserName = dialogView.findViewById(R.id.input_user_name);
+                    final EditText inputUserPassword = dialogView.findViewById(R.id.input_user_password);
+                    final ImageView qr_core = dialogView.findViewById(R.id.qr_code);
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                String apiReturn = getNoContentAPIReturn("https://passport.bilibili.com/qrcode/getLoginUrl");
+                                JSONObject jsonObject = new JSONObject(apiReturn);
+                                JSONObject newJsonObject = jsonObject.getJSONObject("data");
+                                String url = newJsonObject.getString("url");
+                                String oauthKey = newJsonObject.getString("oauthKey");
+                                final Bitmap bitmap = makeQrCore(url);
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
-                                        loggingProgressBar.setVisibility(View.VISIBLE);
-                                        inputUserName.setFocusable(false);
-                                        inputUserPassword.setFocusable(false);
+                                        qr_core.setImageBitmap(bitmap);
                                     }
                                 });
-                                System.out.println("password after encrypt:" + encryption(userPassword));
+                                System.out.println(oauthKey);
+                                keepLoginListen = true;
+                                String LoginInfoReturn;
+                                JSONObject LoginInfoJsonObject;
+                                while (keepLoginListen && !wasLogin) {
+                                    LoginInfoReturn = onLoginListener(oauthKey);
+                                    LoginInfoJsonObject = new JSONObject(LoginInfoReturn);
+                                    boolean status = LoginInfoJsonObject.getBoolean("status");
+                                    wasLogin = status;
+                                    if (wasLogin) {
+                                        toast("已扫描确认!");
+                                        JSONObject userInfoJsonObject = LoginInfoJsonObject.getJSONObject("data");
+                                        String userInfoUrl = userInfoJsonObject.getString("url");
+                                        saveUserInfo(userInfoUrl);
+                                    }
+                                    Thread.sleep(1000);
+                                }
+
+                            } catch (JSONException | InterruptedException e) {
+                                e.printStackTrace();
                             }
-                        }).start();
-                    }
-                });
-                customizeDialog.show();
+                        }
+                    }).start();
+                    loggingButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    String userName = inputUserName.getText().toString();
+                                    String userPassword = inputUserPassword.getText().toString();
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            loggingProgressBar.setVisibility(View.VISIBLE);
+                                            inputUserName.setFocusable(false);
+                                            inputUserPassword.setFocusable(false);
+                                        }
+                                    });
+                                    System.out.println("password after encrypt:" + encryption(userPassword));
+                                }
+                            }).start();
+                        }
+                    });
+                    customizeDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            keepLoginListen = false;
+                        }
+                    });
+                    customizeDialog.show();
+
+                }
             }
         });
     }
@@ -164,32 +224,34 @@ public class MainActivity extends AppCompatActivity {
 
     //带正文api获取返回值
 
-    private String getWithContentAPIReturn(String urlStr,String content) {
-// TODO Auto-generated method stub
+    private String onLoginListener(String oauthKey) {
         StringBuffer document = new StringBuffer();
-        boolean status = false;
         try {
             //链接URL
-            URL url = new URL(urlStr);
-            //返回结果集
-            //创建链接
-            URLConnection conn = url.openConnection();
-            //读取返回结果集
-            conn.setDoOutput(true);
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            conn.setRequestProperty("connection", "keep-alive");
-            conn.setUseCaches(false);//设置不要缓存
-            OutputStreamWriter out =out = new OutputStreamWriter(conn.getOutputStream());
-            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+            URL url = new URL("https://passport.bilibili.com/qrcode/getLoginInfo");
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setRequestMethod("POST"); //发送请求的方式
+            connection.setUseCaches(false); //不使用缓存
+            connection.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded"); //设置服务器解析数据的方式
+            connection.connect();
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(),"UTF-8"));
+            out.write("oauthKey=" + oauthKey);
+            out.flush();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
             String line;
-            out.write(content);
-            out.flush();;
-            while ((line = reader.readLine()) != null) {
-                document.append(line);
+
+            while ((line = in.readLine()) != null) {
+                    document.append(line);
             }
             toast(document.toString());
-            reader.close();
+
             out.close();
+            in.close();
         }catch (UnsupportedEncodingException | MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -214,9 +276,9 @@ public class MainActivity extends AppCompatActivity {
                 for (int x = 0; x < width; x++) {
                     //bitMatrix.get(x,y)方法返回true是黑色色块，false是白色色块
                     if (bitMatrix.get(x, y)) {
-                        pixels[y * width + x] =  Color.BLACK;//黑色色块像素设置
+                        pixels[y * width + x] =  Color.BLACK;
                     } else {
-                        pixels[y * width + x] =  Color.WHITE;// 白色色块像素设置
+                        pixels[y * width + x] =  Color.WHITE;
                     }
                 }
             }
@@ -228,13 +290,92 @@ public class MainActivity extends AppCompatActivity {
         return bitmap;
     }
 
+    private boolean saveUserInfo(String url) {
+        try {
+            UserData userData = new UserData();
+            String params = url.substring(url.indexOf("?") + 1, url.length());
+            userData.DedeUserID = params.substring(params.indexOf("=") + 1,params.indexOf("&"));
+            params = params.substring(params.indexOf("&") + 1, params.length());
+            userData.DedeUserID__ckMd5 = params.substring(params.indexOf("=") + 1,params.indexOf("&"));
+            params = params.substring(params.indexOf("&") + 1, params.length());
+            userData.Expires = params.substring(params.indexOf("=") + 1,params.indexOf("&"));
+            params = params.substring(params.indexOf("&") + 1, params.length());
+            userData.SESSDATA = params.substring(params.indexOf("=") + 1,params.indexOf("&"));
+            params = params.substring(params.indexOf("&") + 1, params.length());
+            userData.bili_jct = params.substring(params.indexOf("=") + 1,params.indexOf("&"));
+            byte[] saveData = (userData.DedeUserID + "\n" + userData.DedeUserID__ckMd5 + "\n" + userData.Expires +"\n" + userData.SESSDATA + "\n" + userData.bili_jct).getBytes();
+            OutputStream outputStream = new FileOutputStream(localFilePath + "userInfo.inf");
+            outputStream.write(saveData);
+            outputStream.close();
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    private UserData readUserInfo(){
+        UserData userData = new UserData();
+        try {
+            InputStream inputStream = new FileInputStream(localFilePath + "userInfo.inf");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            userData.DedeUserID = bufferedReader.readLine();
+            userData.DedeUserID__ckMd5 = bufferedReader.readLine();
+            userData.Expires = bufferedReader.readLine();
+            userData.SESSDATA = bufferedReader.readLine();
+            userData.bili_jct = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return userData;
+    }
 
+    private void requestMyPermissions() {
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+        }
+    }
 
+    private boolean testLogin(String SESSDATA) {
+        StringBuffer document = new StringBuffer();
+        try {
+            URL url = new URL("https://api.bilibili.com/x/web-interface/nav");
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setDoInput(true);
+            connection.setRequestMethod("GET");
+            connection.setUseCaches(false);
+            connection.setRequestProperty("Cookie", "SESSDATA=" + SESSDATA);
+            connection.connect();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+            String line;
+            while ((line = in.readLine()) != null) {
+                document.append(line);
+            }
+            in.close();
+            JSONObject jsonObject = new JSONObject(document.toString());
+            JSONObject newJsonObject = jsonObject.getJSONObject("data");
+            return newJsonObject.getBoolean("isLogin");
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
+    private void showUserFace(){
 
-
-
+    }
 
 
 
